698a778c8ec2005f4614bdbef560222527aa7b5f
jhoelss <105899191+jhoelss@users.noreply.github.com>
2022-06-09 20:47:53 -0500

    Add files via upload

---
+++ Controllers/PrestamoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tienda.Data;
+using Tienda.Logic;
+
+namespace Financiera.AppWeb.Controllers
+{
+    public class PrestamoController : Controller
+    {
+        // GET: Prestamo
+        public ActionResult Index()
+        {
+            var listado = PrestamoBL.Listar();
+            return View(listado);
+           
+        }
+        //public ActionResult Crear()
+        //{
+        //    return View();
+
+        //}
+        //[HttpPost]
+        //public ActionResult Crear(PrestamoBL prestamo)
+        //{
+        //    using (var db = new FinancieraEntities())
+        //    {
+        //        ;
+        //        db.Prestamo.Add(prestamo);
+        //        db.SaveChanges();
+        //    }
+        //    return View("index");
+
+        //}
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Financiera.AppWeb.MvcApplication" Language="C#" %>
