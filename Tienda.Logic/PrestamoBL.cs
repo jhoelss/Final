a9c81e6a549fece9c80cd774d25f6ce1f134e609
jhoelss <105899191+jhoelss@users.noreply.github.com>
2022-06-09 20:57:20 -0500

    Add files via upload

---
+++ Tienda.Logic/PrestamoBL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.Core;
+using Tienda.Data;
+
+namespace Tienda.Logic
+{
+    public class PrestamoBL
+    {
+        public static List<Prestamo> Listar()
+        {
+            using (var db = new FinancieraEntities())
+            {
+                return db.Prestamo.ToList();
+            }
+
+          
+        }
+        public static Prestamo Buscar(int id)
+        {
+            using (var db = new FinancieraEntities())
+            {
+                return db.Prestamo.Find(id);
+
+            }
+
+        }
+    }
+}
